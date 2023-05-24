a883d69f29ead1d6312c721cbb6845aafb63976f
Emily <emilycoco@me.com>
2023-05-24 12:03:20 +1200

    contact back

--- client/components/Contact.tsx
+++ client/components/Contact.tsx
@@ -3,8 +3,7 @@ function Contact () {
 
   return (
     <>
-    hi
-{/* <div className="flex items-center min-h-screen bg-gray-50 dark:bg-gray-900">
+<div className="flex items-center min-h-screen bg-gray-50 dark:bg-gray-900">
   <div className="container mx-auto">
     <div className="max-w-md mx-auto my-10 bg-white p-5 rounded-md shadow-sm">
       <div className="text-center">
@@ -46,7 +45,7 @@ function Contact () {
       </div>
     </div>
   </div>
-</div> */}
+</div>
 
     </>
   )
